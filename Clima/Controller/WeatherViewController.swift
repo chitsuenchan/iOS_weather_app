a21bdd7fd439ec78430b925a4a14eff221114794
chi chan <chitsuenchan@gmail.com>
2023-07-02 20:57:39 +0800

    added initial files (can connect to api

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,70 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManagerDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    var weatherManager = WeatherManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        weatherManager.delegate = self
+        searchTextField.delegate = self
     }
 
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true) // when the user clicks the search icon it will dismiss the keyboard
+    }
+    
+    // This function below enables us to retrieve something after the user click "go" or "enter"
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true) // When the user clicks search on the keyboard it dismisses the keyboard
+        return true
+    }
+    
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if (textField.text != "") {
+            return true     // if true then it will end editing text field
+        } else {
+            textField.placeholder = "Type something"    // changes the place holder
+            return false        // will not end editing
+        }
+    }
+    
+    // This is triggered when editing is done
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        if let city = searchTextField.text {
+            weatherManager.fetchWeather(cityName: city)
+        }
+        
+        searchTextField.text = ""   // resets the text input field
+    }
+    
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        
+        /*
+         The data contained in weather.temperatureString is completely dependent if the networking task has been completed in WeatherManager.
+         It could take from a few seconds to a few minutes so it would freeze the UI because it can't set the text as there is nothing to set with and so all the other UI buttons will stop working on the main thread.
+         
+         The solution is to wrap the code below with the DispatchQueue to call the main thread to update the UI once its completed.
+         Note, since it is a closure function we have to add self.
+         */
+        DispatchQueue.main.async{
+            self.temperatureLabel.text = weather.temperatureString  // updating the text label
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+        }
 
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+    
 }
-
---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,86 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Chi  Chan on 2/7/2023.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+protocol WeatherManagerDelegate {
+    
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel)
+    func didFailWithError(error: Error)
+}
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=1e1494de00c762a08ac857ac46467ac1&units=metric"
+    
+    var delegate: WeatherManagerDelegate?
+    
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        self.performRequest(with: urlString)
+    }
+    
+    func performRequest(with urlString: String){
+        
+        // 1. Create a URL
+        /*
+         The URL is optional as the urlString could be "" so we should use if let
+         */
+        if let url = URL(string: urlString) {
+            
+            // 2. Create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            // 3. Give the session a task
+            /*
+             A completionHandler is what happens when we get the response. It takes a function.
+             This doesn't have any data in the arguments because once the task is completed with making the API
+             It will then call the handle function and populate it with the arguements (data, response and error)
+             */
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    return      // return with nothing means break out of the function
+                }
+                
+                if let safeData = data {
+                    if let weather = self.parseJSON(safeData) {
+                        
+                        // Once we get a response with some data then we call the didUpdateWeather function and send over the weather data
+                        self.delegate?.didUpdateWeather(self, weather: weather)
+                    }
+                }
+                
+            }
+            
+            // 4. Start the task
+            task.resume()
+        }
+        
+    }
+    
+    func parseJSON(_ weatherData: Data)  -> WeatherModel? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            
+            return weather
+            
+        } catch {
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
+    
+
+}
